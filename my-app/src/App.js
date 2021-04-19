78b962b56353b17f9b34f9bf39ca22c8ac605a25
Anna <garsinaanna32778@gmail.com>
2021-04-19 18:04:44 +0300

    renewed logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,9 @@
-import React from 'react';
 import './App.css';
-import Image1 from "./images/1.jpg";
-import Image2 from "./images/2.jpg";
-import Image3 from "./images/3.jpg";
-import Image4 from "./images/4.jpg";
-import Image5 from "./images/5.jpg";
-import Image6 from "./images/6.jpg";
-import Image7 from "./images/7.jpg";
-import Image8 from "./images/8.jpg";
+import ImageCarousel from './components/ImageCarousel';
+import { CarouselData } from './components/CarouselData';
 
-
-import { Carousel } from './components/Carousel';
-
-class App extends React.Component {
-  state = {
-    images: [
-      Image1,
-      Image2,
-      Image3,
-      Image4,
-      Image5,
-      Image6,
-      Image7,
-      Image8
-    ],
-  };
-
-  render() {
-    const { images } = this.state;
-
-    return (
-      <div className="App">
-        <h1 className="App__header"> Types of sports (in carousel)</h1>
-        <Carousel
-          images={images}
-          imageWidth={130}
-          imagesScrolled={1}
-          imagesDisplayed={1}
-          animationDuration={1000}
-        />
-      </div>
-    );
-  }
+function App() {
+  return <ImageCarousel slides={CarouselData} />;
 }
 
 export default App;
---
+++ my-app/src/components/ImageCarousel.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { CarouselData } from './CarouselData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+
+const ImageCarousel = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <>
+    <h1 class="title">Types of sports:</h1>
+    <section className='slider'>
+      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+      {CarouselData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide} alt='sport image' className='image' />
+            )}
+          </div>
+        );
+      })}
+    </section>
+    </>
+  );
+};
+
+export default ImageCarousel;
