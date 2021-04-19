7418194807b3ac0fec55a46e355aa1213edb203c
Anna <garsinaanna32778@gmail.com>
2021-04-19 16:18:56 +0300

    imported images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
+import Image1 from "./images/1.jpg";
+import Image2 from "./images/2.jpg";
+import Image3 from "./images/3.jpg";
+import Image4 from "./images/4.jpg";
+import Image5 from "./images/5.jpg";
+import Image6 from "./images/6.jpg";
+import Image7 from "./images/7.jpg";
+import Image8 from "./images/8.jpg";
+
 
 import { Carousel } from './components/Carousel';
 
 class App extends React.Component {
   state = {
     images: [
-      './images/cycling.jpg',
-      './images/football.jpg',
-      './images/gymnastics.jpg',
-      './images/cycling.jpg',
-      './images/ice-skating.jpg',
-      './images/karate.jpg',
-      './images/shooting.jpg',
-      './images/tennis.jpg',
-      './images/voleyball.jpg',
+      Image1,
+      Image2,
+      Image3,
+      Image4,
+      Image5,
+      Image6,
+      Image7,
+      Image8
     ],
   };
 
